47d98751f52d0ac447c2355f00be2869734bdfa0
stephannils <steniljeh@gmail.com>
2020-09-05 05:14:50 +0200

    first commit

---
+++ src/BoxContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Box from './Box';
+
+class BoxContainer extends Component {
+  static defaultProps = {
+    colors: ['red', 'blue', 'white', 'black'],
+  };
+  render() {
+    return (
+      <div className="BoxContainer">
+        {this.props.colors.map((c) => (
+          <Box color={c} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default BoxContainer;
