0de7d9803dcc20f1629dbf14ed0c4618d9568222
stephannils <steniljeh@gmail.com>
2020-09-05 05:42:16 +0200

    finished mini project with stateful box component (child is smarter than parent)

--- src/Box.js
+++ src/Box.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import './Box.css';
 
+const colors = [
+  'red',
+  'blue',
+  'black',
+  'green',
+  'purple',
+  'orange',
+  'gray',
+  'magenta',
+];
+
 class Box extends Component {
+  state = {
+    color: colors[Math.floor(Math.random() * colors.length)],
+  };
+
+  handleClick = (e) => {
+    this.setState({ color: colors[Math.floor(Math.random() * colors.length)] });
+  };
+
   render() {
-    return <div className="Box" style={{ background: this.props.color }}></div>;
+    return (
+      <div
+        className="Box"
+        style={{
+          background: this.state.color,
+        }}
+        onClick={this.handleClick}
+      ></div>
+    );
   }
 }
 
